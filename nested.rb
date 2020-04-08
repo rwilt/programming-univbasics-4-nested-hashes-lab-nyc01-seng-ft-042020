08a812744dd99c2364f2c6da459e7192fc4951c4
rosie w <rosie.wilt@gmail.com>
2020-04-08 19:54:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -36,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:known_for]
 end
 
 def adding_matz
@@ -81,6 +80,15 @@ def adding_matz
         :languages => ["C"]
       }
     }
+		
+programmer_hash[:yukihiro_matsumoto] => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+programmer_hash[:Matz] => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+}
 
 end
 
@@ -103,12 +111,13 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer hash
 
 end
 
+
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -125,5 +134,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:known_for] = "Assembly"
+programmer_hash
 end
+
